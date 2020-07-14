6588569dcfd66b493f9d7202a434fc65f6addd75
hoangtularva <hoangtukg295@gmail.com>
2020-07-14 11:53:54 +0700

    migrate room and room message

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -1,4 +1,4 @@
 class RoomMessage < ApplicationRecord
-  belongs_to :room
+  belongs_to :room, inverse_of: :room_messages
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_040206) do
+ActiveRecord::Schema.define(version: 2020_07_14_045227) do
+
+  create_table "room_messages", force: :cascade do |t|
+    t.integer "room_id", null: false
+    t.integer "user_id", null: false
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_messages_on_room_id"
+    t.index ["user_id"], name: "index_room_messages_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_rooms_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +43,6 @@ ActiveRecord::Schema.define(version: 2020_07_14_040206) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "room_messages", "rooms"
+  add_foreign_key "room_messages", "users"
 end
